05b22e98d306f283c03a8bdea97b64c7735815c4
Maja <stamenicmaja5@uns.ac.rs>
2019-12-16 19:39:57 +0100

    Inicijalni komit

---
+++ src/projekat/MyApp.java
@@ -0,0 +1,15 @@
+package projekat;
+
+import java.awt.Dimension;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		MainFrame mainframe = new MainFrame();
+		mainframe.setVisible(true);
+		mainframe.setSize(new Dimension(500, 300));
+		mainframe.setLocationRelativeTo(null);
+	}
+
+}
