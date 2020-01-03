1de5d00ae4a4773fd127d693a93c1303a8d7bad7
tasa1998 <58151458+tasa1998@users.noreply.github.com>
2020-01-03 12:09:18 +0100

    StudentiTabela

--- src/tabela/StudentiTabela.java
+++ src/tabela/StudentiTabela.java
@@ -1,5 +1,21 @@
 package tabela;
 
-public class StudentiTabela {
+import java.util.ArrayList;
 
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
+import projekat.Student;
+
+public class StudentiTabela extends JTable{
+
+	private static final long serialVersionUID = 1L;
+
+	public StudentiTabela(ArrayList<Student> studenti) {
+		StudentiTabelaModel studentiTabelaModel = new StudentiTabelaModel(studenti);
+		this.setRowSelectionAllowed(true);
+		//this.setColumnSelectionAllowed(true);
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.setModel(studentiTabelaModel);
+	}
 }
--- src/tabela/StudentiTabelaModel.java
+++ src/tabela/StudentiTabelaModel.java
@@ -1,5 +1,84 @@
 package tabela;
 
-public class StudentiTabelaModel {
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.net.ssl.SSLEngineResult.Status;
+import javax.swing.table.AbstractTableModel;
+
+import projekat.Student;
+
+public class StudentiTabelaModel extends AbstractTableModel{
+	
+	private static final long serialVersionUID = 1L;
+
+	private static String[] naziviKolona = {"Ime", "Prezime",  "Datum rodjenja", "Adresa stanovanja", "Telefon", "E-mail","Broj indeksa", "Datum upisa", "Trenutna godina studija","Status studenta","Prosecna ocena", "Spisak predmeta koje slusa"};
+	private static Class[] tipoviKolona = {String.class,String.class,Date.class,String.class,String.class,String.class,String.class,Date.class,Integer.class,Status.class,Double.class, String.class};
+	private ArrayList<Student> studenti = new ArrayList<Student>();
+	
+	public StudentiTabelaModel(ArrayList<Student> studenti) {
+		// TODO Auto-generated constructor stub
+		this.studenti=studenti;
+	}
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return naziviKolona.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return studenti.size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		Student student=studenti.get(rowIndex);
+		
+		switch(columnIndex) {
+		case 0:
+			return student.getIme();
+		case 1:
+			return student.getPrezime();
+		case 2:
+			return student.getDatumRodjenja();
+		case 3:
+			return student.getAdresaStanovanja();
+		case 4:
+			return student.getTelefon();
+		case 5:
+			return student.getEmail();
+		case 6:
+			return student.getBrojIndeksa();
+		case 7:
+			return student.getDatumUpisa();
+		case 8:
+			return student.getGodinaStudija();
+		case 9:
+			return student.getStatus();
+		case 10:
+			return student.getProsecnaOcena();
+		case 11:
+			return "Prikazi predmete";
+		}
+		return null;
+	}
+	
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		return tipoviKolona[columnIndex];
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return naziviKolona[column];
+	}
+	
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		super.setValueAt(aValue, rowIndex, columnIndex);
+	}
 
 }
