52d9aed9290030cfe1a3b52cd12187dd2451961b
Maja <stamenicmaja5@uns.ac.rs>
2020-01-05 21:02:28 +0100

    #uklanjanje_profesora_sa_predmeta Zavrsena funkcionalnost uklanjanja profesora sa predmeta.

--- src/dialozi/DialogUklanjanjeProfesoraSaPredmet.java
+++ src/dialozi/DialogUklanjanjeProfesoraSaPredmet.java
@@ -3,18 +3,36 @@ package dialozi;
 import java.awt.Dimension;
 
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.ListSelectionModel;
 
-public class DialogUklanjanjeProfesoraSaPredmet extends JDialog {
+import projekat.MainFrame;
+import projekat.MyApp;
+import projekat.Predmet;
+
+public class DialogUklanjanjeProfesoraSaPredmet extends JDialog{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public DialogUklanjanjeProfesoraSaPredmet(){
-		setTitle("Uklanjanje profesora sa predmeta");
-		setSize(new Dimension(300, 200));
+	public DialogUklanjanjeProfesoraSaPredmet(int indexUModelu){
 		setLocationRelativeTo(null);
+		String sifra = MyApp.getPredmeti().get(indexUModelu).getSifra();
+		
+		addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				if(JOptionPane.showConfirmDialog(MainFrame.getInstance(), "Da li ste sigurni da zelite da uklonite profesora sa predmeta?",
+						"Potvrda o brisanju profesora sa predmeta", JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+					Predmet.uklanjanjeProfesora(MyApp.getPredmeti().get(indexUModelu).getPredmetniProfesor(), sifra);
+					MainFrame.refreshTabova();
+				}
+				dispose();
+			}
+		});
 		setVisible(true);
 	}
 }
