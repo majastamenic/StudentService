db54b04a1e52e2cc4b3cb6eb0a714ac7deb94313
tasa1998 <58151458+tasa1998@users.noreply.github.com>
2020-01-12 20:53:01 +0100

    #dodavanje_studenta povezivanje predmeta i studenta

--- src/tabela/StudentiTabelaButton.java
+++ src/tabela/StudentiTabelaButton.java
@@ -18,26 +18,25 @@ import projekat.Student;
 
 public class StudentiTabelaButton implements TableCellRenderer, MouseListener {
 
-	// dugme koje se iscrtava
-	private JButton renderButton;
-// referenca na tabelu
-	private JTable table;
+	
+	private JButton renderButton; 	// dugme koje se iscrtava
+
+	private JTable table;  // referenca na tabelu
 
 public StudentiTabelaButton(JTable table, int column) {
-// povezivanje sa tabelom
-this.table = table;
-// nacin iscrtavanje celije
-this.table.getColumnModel().getColumn(11).setCellRenderer(this);
+
+this.table = table;  // povezivanje sa tabelom
+
+this.table.getColumnModel().getColumn(11).setCellRenderer(this);  // nacin iscrtavanje celije
 this.table.addMouseListener(this);
 
-// dugme koje ce biti iscrtavanp
-this.renderButton = new JButton("Prikazi");
+
+this.renderButton = new JButton("Prikazi");	// dugme koje ce biti iscrtavano
 
 this.renderButton.addActionListener(new ActionListener() {
-	// mozemo odavde pozvati nas kontroler da se nesto smisleno odradi
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		Student student = null;
+		Student student = null;	//gledamo koji je tacno student selektovan i uzimamo njegovu vrednost
 		String brojIndeksa = (String) table.getValueAt(table.getSelectedRow(), 6);			// 6 da dobijemo broj indeksa
 		
 		for (Student stud : MyApp.getStudenti()) {
@@ -47,7 +46,7 @@ this.renderButton.addActionListener(new ActionListener() {
 			}
 		}
 		
-		DialogPredmetiUStudentu predmetiKojeSlusa= new DialogPredmetiUStudentu(student);
+		DialogPredmetiUStudentu predmetiKojeSlusa= new DialogPredmetiUStudentu(student); //pozivamo dijalog koji prikazuje predmete koje slusa
 	}
 });
 
@@ -56,9 +55,7 @@ this.renderButton.addActionListener(new ActionListener() {
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
 			int row, int column) {
-// U ovom primeru, sva dugmad ce imati isti ispis,
-// za promenu ispisa mozemo koristi vrednost parametra value
-		//return new IgraciTablePanel(this.renderButton, 50, 50);
+//svi dugmici imaju isti ispis
 		MyButtonPanel mbp = new MyButtonPanel(this.renderButton, value);
 		return mbp;
 	}
@@ -66,7 +63,7 @@ this.renderButton.addActionListener(new ActionListener() {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (table.getSelectedColumn() == 11) {
+		if (table.getSelectedColumn() == 11) {// ako kliknemo dugme koje je u poslednjoj koloni,trazimo koja je kolona broj indeksa jer je to kljuc
 			Student student = null;
 			int kolona = 0;
 			for (int i = 0; i < table.getColumnCount(); i++) {
@@ -75,12 +72,12 @@ this.renderButton.addActionListener(new ActionListener() {
 					break;
 				}
 			}
-			String brojIndeksa = (String) table.getValueAt(table.getSelectedRow(), kolona);			
+			String brojIndeksa = (String) table.getValueAt(table.getSelectedRow(), kolona);	 		//uzimamo vrednost broja indeksa iz te kolone
 			
 			for (Student s : MyApp.getStudenti()) {
 				if (s.getBrojIndeksa().equals(brojIndeksa)) {
 					student = s;
-					break;
+					break;		//nadjemo studenta sa tim brojem indeksa i onda otvaramo njegov dijalog
 				}
 			}
 			
